0261c1b1acc272a112cbf352221bc038e220bbb3
Sallaa <valentin.porcellini@gmail.com>
2018-03-28 12:54:04 +0200

    Filled classes Added Test class

---
+++ src/nn/neuralnet/NeuralNetConsoleTest.java
@@ -0,0 +1,44 @@
+package nn.neuralnet;
+
+import nn.neuralnet.math.IActivationFunction;
+import nn.neuralnet.math.Linear;
+import nn.neuralnet.math.RandomNumberGenerator;
+import nn.neuralnet.math.Sigmoid;
+
+/**
+ *
+ * NeuralNetConsoleTest
+ * This class is solely used for creating and testing your very first Neural 
+ * Network in Java 
+ *
+ * @author Alan de Souza, Fábio Soares
+ */
+public class NeuralNetConsoleTest {
+    public static void main(String[] args){
+        
+        RandomNumberGenerator.seed=0;
+        
+        int numberOfInputs=2;
+        int numberOfOutputs=1;
+        int[] numberOfHiddenNeurons= { 3 };
+        IActivationFunction[] hiddenAcFnc = { new Sigmoid(1.0) } ;
+        Linear outputAcFnc = new Linear(1.0);
+        NeuralNet nn = new NeuralNet(numberOfInputs,numberOfOutputs,
+                numberOfHiddenNeurons,hiddenAcFnc,outputAcFnc);
+        
+        double [] neuralInput = { 1.5 , 0.5 };
+        
+        double [] neuralOutput;
+        nn.setInputs(neuralInput);
+        nn.calc();
+        neuralOutput=nn.getOutputs();
+        
+        neuralInput[0] = 1.0;
+        neuralInput[1] = 2.1;
+        
+        nn.setInputs(neuralInput);
+        nn.calc();
+        neuralOutput=nn.getOutputs();
+        
+    }
+}
